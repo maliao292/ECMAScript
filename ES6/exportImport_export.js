79c4e6dc28d956098b7421072909ea09f53f7642
mario <maming292@163.com>
2018-12-05 13:23:33 +0800

    引入js

---
+++ ES6/exportImport_export.js
@@ -0,0 +1,8 @@
+
+export const sqrt = Math.sqrt;
+export function square(x) {
+	return x * x;
+}
+export function add (x, y) {
+	return x + y;
+}
---
+++ ES6/exportImport_import.js
@@ -0,0 +1,27 @@
+import { square, add } from './exportImport_export.js';
+console.log(square(10)); //100
+console.log(add(2, 4)); //6
+
+
+
+
+// todo 引入方式  type 必须为 module       type="module"
+// todo <script type="module">
+// todo  import {addTextToBody} from './utils.mjs';
+// todo  addTextToBody('Modules are pretty cool.');
+// todo</script>
+
+
+//Supported (支持 的引入方式):
+/*
+import {foo} from 'https://jakearchibald.com/utils/bar.mjs';
+import {foo} from '/utils/bar.mjs';
+import {foo} from './bar.mjs';
+import {foo} from '../bar.mjs';
+*/
+
+// Not supported （不支持 的引入方式）:
+/*
+import {foo} from 'bar.mjs';
+import {foo} from 'utils/bar.mjs';
+*/
